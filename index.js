8e1c10322cf1ee9c7781cc3e4d6f019b05b8758e
Ibrahim Hussain <ih2304@bu.edu>
2025-09-15 23:55:05 -0400

    Initial commit: Complete resume website with calculator functionality

---
+++ index.js
@@ -0,0 +1,6 @@
+let one= document.getElementById('one');
+let two= document.getElementById('two');
+let output=document.getElementById('output');
+function doAdd(){
+   output.innerHTML=String(Number(one.value)+ Number(two.value));
+}
